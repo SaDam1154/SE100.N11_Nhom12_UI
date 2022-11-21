28b53e01b34572f8925a004f252e116057725744
Đam Sa <20521154@ms.uit.edu.vn>
2022-11-21 17:37:30 +0700

    List_product_complete

--- src/pages/AddProduct/index.jsx
+++ src/pages/AddProduct/index.jsx
@@ -1,5 +1,45 @@
 function Addroduct() {
-    return ( <h1>ADD PRODUCT</h1> );
-}
+    return (
+        <div className="flex flex-row">
+            <div className="basis-3/4    flex flex-col">
+                <label class="unit font-bold" for="name">
+                    Tên cây
+                </label>
+                <input
+                    type="text"
+                    id="name"
+                    class="border cursor-pointer border-slate-200 hover:bg-slate-100"
+                    required
+                />
 
+                <label class="unit font-bold" for="type">
+                    Loại cây
+                </label>
+                <input
+                    type="text"
+                    class="border cursor-pointer border-slate-200 hover:bg-slate-100"
+                    id="type"
+                />
+
+                <label class="unit font-bold" for="quantity">
+                    Số lượng
+                </label>
+                <input
+                    type="number"
+                    value="1"
+                    class="border cursor-pointer border-slate-200 hover:bg-slate-100"
+                    id="quantity"
+                    required
+                />
+            </div>
+            <div className="basis-1/4 flex flex-col">
+                <div class="grow text-center bg-zinc-900 text-white">
+                    <h1>HÌNH</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+//
+//
 export default Addroduct;
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,56 +1,91 @@
 const products = [
     {
         num: 1,
-        id: 123,
-        type: 'Cây cảnh',
-        name: 'Xương rồng',
+        id: 1000001,
+        type: "Cây cảnh",
+        name: "Xương rồng",
         price: 450000,
     },
     {
-        num: 1,
-        id: 456,
-        type: 'Cây cảnh',
-        name: 'Xương rồng',
+        num: 2,
+        id: 1000003,
+        type: "Cây Nhật Mạt Hương",
+        name: "Sen đá",
         price: 450000,
     },
     {
-        num: 1,
-        id: 789,
-        type: 'Cây cảnh',
-        name: 'Xương rồng',
+        num: 3,
+        id: 1000003,
+        type: "Cây sen đá kim cương tím",
+        name: "Sen đá",
         price: 450000,
     },
     {
-        num: 1,
-        id: 657,
-        type: 'Cây cảnh',
-        name: 'Xương rồng',
+        num: 4,
+        id: 1000004,
+        type: "Sen đá Giva",
+        name: "Sen đá",
         price: 450000,
     },
     {
-        num: 1,
-        id: 468,
-        type: 'Cây cảnh',
-        name: 'Xương rồng',
+        num: 5,
+        id: 1000005,
+        type: "Cây Bình An",
+        name: "Dây leo",
         price: 450000,
     },
 ];
 
 function Products() {
     return (
-        <div className="main_window__table_tree">
-            <h1 className="text-2xl font-bold text-slate-900">Danh sách cây</h1>
+        <div className="wrapper">
+            <div class="flex">
+                <label className="text-2xl font-bold text-slate-800">
+                    Danh sách cây
+                </label>
+                <button
+                    type="button"
+                    class="flex  mx-2  items-center justify-center rounded px-4 py-1.5 text-gray-800 hover:bg-blue-400"
+                >
+                    <span class="button-icon">
+                        <i class="fa fa-refresh" aria-hidden="true"></i>
+                    </span>
+                    <span class="">Tải lại</span>
+                </button>
+                <div className="flex grow">
+                    <input type="text" class="grow  rounded bg-gray-300" />
+
+                    <button type="button" class="">
+                        <span class="px-4 rounded">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </span>
+                    </button>
+
+                    <button type="button" class="px-5">
+                        <span class="">
+                            <i class="fas fa-filter"></i>
+                        </span>
+                    </button>
+                </div>
+
+                <button className=" flex items-center justify-center rounded bg-green-600 px-4 py-1.5 mr-4 text-white hover:bg-green-400">
+                    <span className="pr-1">
+                        <i className="fa-solid fa-circle-plus"></i>
+                    </span>
+                    <span>Thêm cây mới</span>
+                </button>
+            </div>
             <table className="mt-4 w-full">
                 <colgroup>
-                    <col span="1" style={{ width: '10%' }} />
-                    <col span="1" style={{ width: '20%' }} />
-                    <col span="1" style={{ width: '20%' }} />
-                    <col span="1" style={{ width: '20%' }} />
-                    <col span="1" style={{ width: '20%' }} />
-                    <col span="1" style={{ width: '10%' }} />
+                    <col span="1" style={{ width: "10%" }} />
+                    <col span="1" style={{ width: "20%" }} />
+                    <col span="1" style={{ width: "20%" }} />
+                    <col span="1" style={{ width: "20%" }} />
+                    <col span="1" style={{ width: "20%" }} />
+                    <col span="1" style={{ width: "10%" }} />
                 </colgroup>
 
-                <thead className="h-11 bg-blue-500 text-white">
+                <thead className="h-11 bg-blue-500 text-white rounded">
                     <tr>
                         <th scope="col">STT</th>
                         <th scope="col">Mã số</th>
@@ -68,7 +103,9 @@ function Products() {
                             <td className="py-2 text-center">{product.id}</td>
                             <td className="py-2 text-center">{product.type}</td>
                             <td className="py-2 text-center">{product.name}</td>
-                            <td className="py-2 text-center">{product.price}</td>
+                            <td className="py-2 text-center">
+                                {product.price}
+                            </td>
                             <td className="py-2 text-center">
                                 <div className="flex">
                                     <button className="flex items-center justify-center rounded bg-blue-500 px-4 py-1.5 text-white hover:bg-blue-400">
@@ -79,7 +116,7 @@ function Products() {
                                     </button>
                                     <button className="flex items-center ml-2 justify-center rounded bg-red-500 px-4 py-1.5 text-white hover:bg-red-400">
                                         <span className="pr-1">
-                                            <i className="fa-solid fa-circle-plus"></i>
+                                            <i className="fa-solid fa-circle-xmark"></i>
                                         </span>
                                         <span>Xoá</span>
                                     </button>
