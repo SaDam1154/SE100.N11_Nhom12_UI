d63158bede2efa8f523cfc03292bf37c7f12a491
Đam Sa <20521154@ms.uit.edu.vn>
2022-11-22 14:48:37 +0700

    Customer

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -51,97 +51,94 @@ const customers = [
 ];
 
 function Customers() {
-    return;
-    <div className="wrapper">
-        <div className="flex">
-            <label className="text-2xl font-bold text-slate-800">
-                Danh sách cây
-            </label>
-            <button
-                type="button"
-                className="flex  mx-2  items-center justify-center rounded px-4 py-1.5 text-gray-800 hover:bg-blue-400"
-            >
-                <span className="button-icon">
-                    <i className="fa fa-refresh" aria-hidden="true"></i>
-                </span>
-                <span className="">Tải lại</span>
-            </button>
-            <div className="flex grow">
-                <input type="text" className="grow  rounded bg-gray-300" />
+    return ( 
+        <div>
+            <div className="flex space-x-4">
+                {/* tite + reload btn */}
+                <div className="flex">
+                    <label className="text-2xl font-bold text-slate-800">Danh sách khách hàng</label>
+                    <button type="button" className="ml-3 text-gray-800 hover:underline">
+                        <span className="font-sm pr-1">
+                            <i className="fa fa-refresh" aria-hidden="true"></i>
+                        </span>
+                        <span className="">Tải lại</span>
+                    </button>
+                </div>
 
-                <button type="button" className="">
-                    <span className="px-4 rounded">
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                    </span>
-                </button>
+                {/* Action group */}
+                <div className="flex grow">
+                    {/* Search */}
+                    <div className="mr-2 flex grow">
+                        <input type="text" className="text-input grow" placeholder="Tìm kiếm khách hàng" />
 
-                <button type="button" className="px-5">
-                    <span className="">
+                        <button className="btn btn-md bg-slate-200 !px-3 text-slate-600 hover:bg-slate-300">
+                            <i className="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </div>
+
+                    <button className="btn btn-md bg-slate-200 !px-3 text-slate-600 hover:bg-slate-300">
                         <i className="fas fa-filter"></i>
-                    </span>
-                </button>
+                    </button>
+
+                    <button className="btn btn-md bg-green-600 hover:bg-green-500">
+                        <span className="pr-1">
+                            <i className="fa-solid fa-circle-plus"></i>
+                        </span>
+                        <span>Thêm khách hàng</span>
+                    </button>
+                </div>
             </div>
-            <button className="flex items-center justify-center rounded bg-green-600 px-4 py-1.5 text-white hover:bg-green-500">
-                <span className="pr-1">
-                    <i className="fa-solid fa-circle-plus"></i>
-                </span>
-                <span>Thêm cây mới</span>
-            </button>
-        </div>
-        <table className="mt-4 w-full">
-            <colgroup>
-                <col span="1" style={{ width: "10%" }} />
-                <col span="1" style={{ width: "20%" }} />
-                <col span="1" style={{ width: "20%" }} />
-                <col span="1" style={{ width: "20%" }} />
-                <col span="1" style={{ width: "20%" }} />
-                <col span="1" style={{ width: "10%" }} />
-            </colgroup>
+            <table className="mt-4 w-full">
+                <colgroup>
+                    <col span="1" style={{ width: '10%' }} />
+                    <col span="1" style={{ width: '20%' }} />
+                    <col span="1" style={{ width: '20%' }} />
+                    <col span="1" style={{ width: '20%' }} />
+                    <col span="1" style={{ width: '20%' }} />
+                    <col span="1" style={{ width: '10%' }} />
+                </colgroup>
 
-            <thead className="h-11 bg-blue-500 text-white rounded">
-                <tr>
-                    <th scope="col">STT</th>
-                    <th scope="col">Mã KH</th>
-                    <th scope="col">Tên khách hàng</th>
-                    <th scope="col">Số điện thoại</th>
-                    <th scope="col">Địa chỉ</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-
-            <tbody className="list_tree_body">
-                {products.map((product) => (
-                    <tr
-                        key={product.id}
-                        className="border-b cursor-pointer border-slate-200 hover:bg-slate-100"
-                    >
-                        <td className="py-2 text-center">{product.num}</td>
-                        <td className="py-2 text-center">{product.id}</td>
-                        <td className="py-2 text-center">{product.name}</td>
-                        <td className="py-2 text-center">{product.phone}</td>
-                        <td className="py-2 text-center">{product.address}</td>
-
-                        <td className="py-2 text-center">
-                            <div className="flex">
-                                <button className="flex items-center justify-center rounded bg-blue-500 px-4 py-1.5 text-white hover:bg-blue-400">
-                                    <span className="pr-1">
-                                        <i className="fa-solid fa-circle-plus"></i>
-                                    </span>
-                                    <span>Sửa</span>
-                                </button>
-                                <button className="flex items-center ml-2 justify-center rounded bg-red-500 px-4 py-1.5 text-white hover:bg-red-400">
-                                    <span className="pr-1">
-                                        <i className="fa-solid fa-circle-xmark"></i>
-                                    </span>
-                                    <span>Xoá</span>
-                                </button>
-                            </div>
-                        </td>
+                <thead className="h-11 rounded bg-blue-500 text-white">
+                    <tr>
+                        <th scope="col">STT</th>
+                        <th scope="col">Mã KH</th>
+                        <th scope="col">Tên khách hàng</th>
+                        <th scope="col">Số điện thoại</th>
+                        <th scope="col">Địa chỉ </th>
+                        <th scope="col"></th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>;
+                </thead>
+
+                <tbody>
+                    {customers.map((customers) => (
+                        <tr key={customers.id} className="cursor-pointer border-b border-slate-200 hover:bg-slate-100">
+                            <td className="py-2 text-center">{customers.num}</td>
+                            <td className="py-2 text-center">{customers.id}</td>
+                            <td className="py-2 text-center">{customers.name}</td>
+                            <td className="py-2 text-center">{customers.phone}</td>
+                            <td className="py-2 text-center">{customers.address}</td>
+                            <td className="py-2 text-center">
+                                <div className="flex">
+                                    <button className="btn btn-sm bg-blue-500 hover:bg-blue-400">
+                                        <span className="pr-1">
+                                            <i class="fa-solid fa-pen-to-square"></i>
+                                        </span>
+                                        <span>Sửa</span>
+                                    </button>
+                                    <button className="btn btn-sm bg-red-500 hover:bg-red-400">
+                                        <span className="pr-1">
+                                            <i className="fa-solid fa-circle-xmark"></i>
+                                        </span>
+                                        <span>Xoá</span>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Customers;
