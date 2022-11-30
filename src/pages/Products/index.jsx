c4ff14a56b4f611811af1d1d61deda818e33f9aa
Minh Chau <buitongminhchau01032002@gmail.com>
2022-11-30 13:56:20 +0700

    fix dont have type product in product list page

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -177,7 +177,7 @@ function Products() {
                             <td className="py-2 text-center">{index + 1}</td>
                             <td className="py-2 text-center">{product.id}</td>
                             <td className="py-2 text-center">
-                                {product.type.name}
+                                {product.type?.name || '-'}
                             </td>
                             <td className="py-2 text-center">{product.name}</td>
                             <td className="py-2 text-center">
