c5bc3d07ce5472d9a31b3f752fb02a80ce259dee
Đam Sa <20521154@ms.uit.edu.vn>
2022-12-04 15:32:12 +0700

    ListBox Addproduct

--- src/pages/AddProduct/index.jsx
+++ src/pages/AddProduct/index.jsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Fragment, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Listbox, Popover } from '@headlessui/react';
+import clsx from 'clsx';
+import { useEffect } from 'react';
 
 function Addroduct() {
     const [img, setImg] = useState();
@@ -46,6 +49,27 @@ function Addroduct() {
         });
     };
 
+    //SaDam load typeProduct
+    const [productTypes, setProductTypes] = useState([]);
+
+    const [selectedProductType, setSelectedProductType] = useState({});
+    useEffect(() => {
+        callApiProductTypes();
+    }, []);
+
+    function callApiProductTypes() {
+        fetch('http://localhost:5000/api/product-type')
+            .then((res) => res.json())
+            .then((resJson) => {
+                if (resJson.success) {
+                    setProductTypes(resJson.productTypes);
+                } else {
+                    setProductTypes([]);
+                }
+            });
+    }
+    //end load type product
+
     return (
         <div className="container">
             <div className="w-full">
@@ -70,7 +94,53 @@ function Addroduct() {
                                 />
                                 {/* <span className="form-message">Vui lòng nhập tên cây</span> */}
                             </div>
+                            <div className="mt-3 space-x-2">
+                                <div>
+                                    <Listbox
+                                        value={selectedProductType}
+                                        onChange={setSelectedProductType}
+                                    >
+                                        <Listbox.Button
+                                            as="div"
+                                            className="text-input flex min-h-[36px] cursor-pointer items-center"
+                                        >
+                                            <div className="mr-2 flex-1">
+                                                {selectedProductType.name ||
+                                                    'Loại cây'}{' '}
+                                            </div>
+                                            <i className="fa-solid fa-chevron-down"></i>
+                                        </Listbox.Button>
+                                        <Listbox.Options>
+                                            {productTypes.map((type) => (
+                                                <Listbox.Option
+                                                    key={type._id}
+                                                    value={type}
+                                                    className="cursor-pointer hover:text-blue-500"
+                                                >
+                                                    {({ selected }) => (
+                                                        <div className="flex items-center">
+                                                            <i
+                                                                className={clsx(
+                                                                    'fa-solid fa-check pr-2',
+                                                                    {
+                                                                        'opacity-0':
+                                                                            !selected,
+                                                                    }
+                                                                )}
+                                                            ></i>
+                                                            <span>
+                                                                {type.name}
+                                                            </span>
+                                                        </div>
+                                                    )}
+                                                </Listbox.Option>
+                                            ))}
+                                        </Listbox.Options>
+                                    </Listbox>
+                                </div>
+                            </div>
 
+                            {/*  
                             <div className="form-group flex flex-col">
                                 <label
                                     className="mb-1 font-semibold"
@@ -87,8 +157,9 @@ function Addroduct() {
                                     value={formdata.type}
                                     required
                                 />
-                                {/* <span className="form-message">Vui chọn loại cây</span> */}
-                            </div>
+                                <span className="form-message">Vui chọn loại cây</span>
+                        </div> 
+                             */}
 
                             <div className="form-group flex flex-col">
                                 <label
