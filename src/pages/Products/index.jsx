d15a31ebfe7439abe50f20abb63814f2e6b1418e
Đam Sa <20521154@ms.uit.edu.vn>
2022-12-08 02:33:06 +0700

    Search tieng viet khong dau in products

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -6,7 +6,38 @@ import { useEffect } from 'react';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+function removeVietnameseTones(stra) {
+    var str = stra;
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e');
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i');
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o');
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u');
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y');
+    str = str.replace(/đ/g, 'd');
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, 'A');
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, 'E');
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, 'I');
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, 'O');
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, 'U');
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, 'Y');
+    str = str.replace(/Đ/g, 'D');
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ''); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ''); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+    // Remove extra spaces
+    // Bỏ các khoảng trắng liền nhau
+    str = str.replace(/ + /g, ' ');
+    str = str.trim();
+    // Remove punctuations
+    // Bỏ dấu câu, kí tự đặc biệt
+    str = str.replace(
+        /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
+        ' '
+    );
+    return str;
+}
 function Products() {
     const [search, setSearch] = useState('');
     const [loading, setLoading] = useState(false);
@@ -95,6 +126,11 @@ function Products() {
                             className="text-input grow"
                             onChange={(e) => {
                                 setSearch(e.target.value);
+                                console.log(
+                                    removeVietnameseTones(
+                                        e.target.value
+                                    ).toLowerCase()
+                                );
                             }}
                             placeholder="Tìm kiếm sản phẩm"
                         />
@@ -206,12 +242,22 @@ function Products() {
                 >
                     {products
                         .filter((product) => {
-                            return search.toLowerCase() === ''
-                                ? product
-                                : product.name.toLowerCase().includes(search) ||
-                                      product?.type.name
-                                          .toLowerCase()
-                                          .includes(search);
+                            if (search === '') {
+                                return product;
+                            } else {
+                                if (
+                                    removeVietnameseTones(
+                                        product.name.toLowerCase()
+                                    ).includes(
+                                        removeVietnameseTones(
+                                            search.toLowerCase()
+                                        )
+                                    )
+                                ) {
+                                    var id = product.id.toString();
+                                    return product.id.toString().includes(id);
+                                }
+                            }
                         })
                         ?.map((product, index) => (
                             <tr
