06e9bc4e6676032f138c3c87d74722c5240d1255
qv2002 <20520866@gm.uit.edu.vn>
2022-12-15 14:25:17 +0700

    update detailtree

--- src/pages/DetailTree/index.jsx
+++ src/pages/DetailTree/index.jsx
@@ -45,37 +45,28 @@ function DetailTree() {
     return (
         <div className="container">
             <div className="w-full">
-                <div className="mt-4 flex flex-row">
+                <div className=" flex flex-row mt-4">
                     <div className="mr-8 mt-3 flex w-1/2 flex-col space-y-4 text-lg">
+                        <div className="form-group flex flex-col mt-10">
+                            <label className="mb-1 font-semibold cursor-default " htmlFor="name">
+                                Mã sản phẩm
+                            </label>
+                            <div id="name" className="text-input disabled select-none py-[5px]">
+                                {product.id}
+                            </div>
+                        </div>
                         <div className="form-group flex flex-col ">
-                            <label className="mb-1 font-semibold" htmlFor="name">
-                                Tên cây{' '}
+                            <label className="mb-1 font-semibold cursor-default " htmlFor="name">
+                                Tên cây
                             </label>
                             <div id="name" className="text-input disabled select-none py-[5px]">
                                 {product.name}
                             </div>
                         </div>
-                        <div className="form-group flex flex-col">
-                            <label className="mb-1 font-semibold" htmlFor="type">
-                                Loại cây
-                            </label>
-                            <div id="name" className="text-input disabled  select-none py-[5px]">
-                                {product?.type?.name}
-                            </div>
-                        </div>
-
-                        <div className="form-group flex flex-col">
-                            <label className="mb-1 font-semibold" htmlFor="quantity">
-                                Số lượng
-                            </label>
-                            <div id="quantity" className="text-input disabled py-[5px]">
-                                {product.quantity}
-                            </div>
-                        </div>
                     </div>
 
                     <div className="form-group w-1/2 flex-col items-center justify-items-center ">
-                        <label className="mb-1 font-semibold" htmlFor="quantity">
+                        <label className="mb-1 font-semibold cursor-default text-lg" htmlFor="quantity">
                             Hình ảnh
                         </label>
                         <div className="h-60 w-full rounded-xl border-2 border-dashed border-cyan-300 bg-gray-100">
@@ -92,20 +83,40 @@ function DetailTree() {
 
                 <div className="mt-4 flex flex-row">
                     <div className="form-group mr-4 mt-3 flex basis-1/2 flex-col">
-                        <label className="mb-1 text-xl font-semibold" htmlFor="date">
+                        <label className="mb-1 font-semibold cursor-default text-lg" htmlFor="type">
+                            Loại cây
+                        </label>
+                        <div id="name" className="text-input disabled  select-none py-[5px]">
+                            {product?.type?.name}
+                        </div>
+                    </div>
+
+                    <div className="form-group ml-4 mt-3 flex basis-1/2 flex-col">
+                        <label className="font-semibold cursor-default text-lg" htmlFor="quantity">
+                            Số lượng
+                        </label>
+                        <div id="quantity" className="ml-lg text-input disabled py-[5px]">
+                            {product.quantity}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mt-2 flex flex-row">
+                    <div className="form-group mr-4 mt-3 flex basis-1/2 flex-col">
+                        <label className="mb-1 text-lg font-semibold cursor-default" htmlFor="date">
                             Ngày nhập cây
                         </label>
-                        <div className="text-input py-[5px] text-xl">
+                        <div className="text-input py-[5px] text-xl disabled">
                             {moment(product.createdAt).format('HH:mm:ss DD/MM/YYYY ')}
                         </div>
                     </div>
 
                     <div className="ml-4 mt-3 flex basis-1/2 flex-col">
-                        <label className="mb-1 text-xl font-semibold" htmlFor="price">
+                        <label className="mb-1 text-lg font-semibold cursor-default" htmlFor="price">
                             Giá
                         </label>
                         <div className="relative">
-                            <div id="price" className="text-input disabled w-full py-[5px]">
+                            <div id="price" className="text-input disabled select-none w-full py-[5px]">
                                 {product.price}
                             </div>
                             <label
@@ -119,13 +130,22 @@ function DetailTree() {
                 </div>
 
                 <div className="float-right mt-8 flex  flex-row">
-                    <div className="float-right flex basis-1 flex-col">
-                        <Link to={'/product'} className="btn btn-blue btn-md w-ful">
+                    <div className="float-left flex basis-1 flex-col">
+                        <Link to={'/product'} className="btn btn-blue btn-md w-ful mr-10">
                             <span className="pr-2">
                                 <i className="fa-solid fa-circle-xmark"></i>
                             </span>
                             <span>Quay lại</span>
                         </Link>
+                    </div>
+                    <div className="float-right flex basis-1 flex-col">
+                        <Link to={'/product/update/' + product.id} className="btn btn-md btn-green">
+                            <span className="pr-2">
+                                <i className="fa fa-share" aria-hidden="true"></i>
+                            </span>
+                            <span>Chỉnh sửa</span>
+                        </Link>
+
                     </div>
                 </div>
             </div>
