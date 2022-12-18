f0f5d5aeaae984932ecd4f1c8c784e467462f7c7
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-18 15:28:52 +0700

    add filter product

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,14 +1,11 @@
 import { Fragment, useState } from 'react';
-import * as Yup from 'yup';
 import { Link, useNavigate } from 'react-router-dom';
-import { Listbox, Popover } from '@headlessui/react';
-import clsx from 'clsx';
 import { useEffect } from 'react';
-import { Formik, useFormik } from 'formik';
-import PriceInput from '../../components/PriceInput';
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Filter from './Filter';
+import PriceFormat from '../../components/PriceFormat';
+
 function removeVietnameseTones(stra) {
     var str = stra;
     str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
@@ -39,32 +36,13 @@ function removeVietnameseTones(stra) {
     return str;
 }
 
-const validationSchema = Yup.object({
-    name: Yup.string().required('Trường này bắt buộc'),
-    price: Yup.number().required('Trường này bắt buộc').min(1, 'Giá phải lớn hơn 0'),
-    quantityStart: Yup.number().required('Trường này bắt buộc').min(1, 'Số lượng phải lớn hơn 0'),
-    quantityEnd: Yup.number().required('Trường này bắt buộc').min(0, 'Số lượng phải lớn hơn '),
-    type: Yup.string().required('Trường này bắt buộc'),
-});
 function Products() {
     const [search, setSearch] = useState('');
-    const [loading, setLoading] = useState(false);
     const [products, setProducts] = useState([]);
-    const [productTypes, setProductTypes] = useState([]);
+    const [filters, setFilters] = useState({});
     const navigate = useNavigate();
     const showDeleteNoti = () => toast.info('Xóa sản phẩm thành công!');
     const showErorrNoti = () => toast.error('Có lỗi xảy ra!');
-    const bacsicForm = useFormik({
-        initialValues: {
-            name: '',
-            priceStart: '',
-            priceEnd: '',
-            quantityStart: '',
-            quantityEnd: '',
-        },
-        validationSchema,
-        onSubmit: handleFormsubmit,
-    });
 
     function deleteProduct(id) {
         fetch('http://localhost:5000/api/product/' + id, {
@@ -84,14 +62,9 @@ function Products() {
                 showErorrNoti();
             });
     }
-    const [selectedProductTypes, setSelectedProductTypes] = useState([]);
-    useEffect(() => {
-        callApi();
-        callApiPeople();
-    }, []);
 
-    function callApi() {
-        fetch('http://localhost:5000/api/product')
+    useEffect(() => {
+        fetch('http://localhost:5000/api/product?' + `filters=${JSON.stringify(filters)}`)
             .then((res) => res.json())
             .then((resJson) => {
                 if (resJson.success) {
@@ -100,52 +73,10 @@ function Products() {
                     setProducts([]);
                 }
             });
-    }
-    function callApiPeople() {
-        fetch('http://localhost:5000/api/product-type')
-            .then((res) => res.json())
-            .then((resJson) => {
-                if (resJson.success) {
-                    setProductTypes(resJson.productTypes);
-                } else {
-                    setProductTypes([]);
-                }
-            });
-    }
+    }, [filters]);
 
     function linkToDetail(id) {
         navigate('/product/detail/' + id);
-    }
-    function linkToUpdate(id) {
-        navigate('/product/update/' + id);
-    }
-    function handleFormsubmit(values) {
-        setLoading(true);
-        fetch('http://localhost:5000/api/product', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(values),
-        })
-            .then((res) => res.json())
-            .then((resJson) => {
-                if (resJson.success) {
-                    setLoading(false);
-                    showSuccessNoti();
-                    setTimeout(() => {
-                        navigate('/product');
-                    }, 4000);
-                    // navigate('/product');
-                } else {
-                    setLoading(false);
-                    showErorrNoti();
-                }
-            })
-            .catch(() => {
-                setLoading(false);
-                showErorrNoti();
-            });
     }
 
     return (
@@ -176,167 +107,9 @@ function Products() {
                         />
                     </div>
 
-                    <Popover className="relative mr-2">
-                        <Popover.Button className="btn btn-md h-full !min-w-0 bg-slate-200 !px-3 text-slate-600 outline-none hover:bg-slate-300">
-                            <i className="fas fa-filter"></i>
-                        </Popover.Button>
+                    {/* FILTER */}
+                    <Filter onChange={(f) => setFilters({ ...f })} hasFilters={Object.keys(filters).length > 0} />
 
-                        <Popover.Panel
-                            as="div"
-                            className="absolute right-0 z-10 min-w-[280px] max-w-[320px] rounded border bg-white px-4 py-3 shadow"
-                        >
-                            <h2 className="mb-2 text-lg font-semibold">Lọc sản phẩm</h2>
-
-                            <hr />
-                            <div className="mt-3 space-x-2">
-                                <div>
-                                    <label className="mb-1 font-semibold">Loại cây</label>
-                                    <Listbox value={selectedProductTypes} onChange={setSelectedProductTypes} multiple>
-                                        <Listbox.Button
-                                            as="div"
-                                            className="text-input flex min-h-[36px] cursor-pointer items-center"
-                                        >
-                                            <div className="mr-2 flex-[1]">{`Loại cây (${selectedProductTypes.length})`}</div>
-                                            <i className="fa-solid fa-chevron-down"></i>
-                                        </Listbox.Button>
-                                        <Listbox.Options>
-                                            {productTypes.map((type) => (
-                                                <Listbox.Option
-                                                    key={type._id}
-                                                    value={type}
-                                                    className="cursor-pointer hover:text-blue-500"
-                                                >
-                                                    {({ selected }) => (
-                                                        <div className="flex items-center">
-                                                            <i
-                                                                className={clsx('fa-solid fa-check pr-2', {
-                                                                    'opacity-0': !selected,
-                                                                })}
-                                                            ></i>
-                                                            <span>{type.name}</span>
-                                                        </div>
-                                                    )}
-                                                </Listbox.Option>
-                                            ))}
-                                        </Listbox.Options>
-                                    </Listbox>
-                                </div>
-                            </div>
-                            <div className="mt-2 ">
-                                <div>
-                                    <label className=" font-semibold" htmlFor="quantityStart">
-                                        Khoảng giá
-                                    </label>
-                                    <div className="mt-1 ml-1 flex ">
-                                        <div className="flex basis-1/2 flex-col px-1">
-                                            <label className=" text-sm font-thin" htmlFor="priceStart">
-                                                từ
-                                            </label>
-                                            <PriceInput
-                                                id="priceStart"
-                                                onChange={bacsicForm.handleChange}
-                                                onBlur={bacsicForm.handleBlur}
-                                                value={bacsicForm.values.priceStart}
-                                                error={bacsicForm.errors.priceStart}
-                                                touched={bacsicForm.touched.priceStart}
-                                                name="priceStart"
-                                                placeholder="Từ"
-                                            />
-                                        </div>
-
-                                        <div className="flex basis-1/2 flex-col px-1">
-                                            <label className=" text-sm font-thin" htmlFor="priceEnd">
-                                                đến
-                                            </label>
-                                            <PriceInput
-                                                id="priceEnd"
-                                                onChange={bacsicForm.handleChange}
-                                                onBlur={bacsicForm.handleBlur}
-                                                value={bacsicForm.values.priceEnd}
-                                                error={bacsicForm.errors.priceEnd}
-                                                touched={bacsicForm.touched.priceEnd}
-                                                name="priceEnd"
-                                                placeholder="Đến"
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="mt-3 ">
-                                <label className=" font-semibold" htmlFor="quantity">
-                                    Số lượng
-                                </label>
-                                <div className="mt-1 ml-1 flex ">
-                                    <div className="flex basis-1/2 flex-col px-1">
-                                        <label className=" text-sm font-thin" htmlFor="priceStart">
-                                            từ
-                                        </label>
-                                        <input
-                                            type="number"
-                                            id="quantityStart"
-                                            className={clsx('text-input w-full py-[5px]', {
-                                                invalid:
-                                                    bacsicForm.touched.quantityStart && bacsicForm.errors.quantityStart,
-                                            })}
-                                            onChange={bacsicForm.handleChange}
-                                            onBlur={bacsicForm.handleBlur}
-                                            value={bacsicForm.values.quantityStart}
-                                            name="quantityStart"
-                                            placeholder="từ"
-                                        />
-                                        <span
-                                            className={clsx('text-sm text-red-500 opacity-0', {
-                                                'opacity-100':
-                                                    bacsicForm.touched.quantityStart && bacsicForm.errors.quantityStart,
-                                            })}
-                                        >
-                                            {bacsicForm.errors.quantityStart || 'No message'}
-                                        </span>
-                                    </div>
-                                    <div className="flex basis-1/2 flex-col px-1">
-                                        <label className=" text-sm font-thin" htmlFor="quantityEnd">
-                                            đến
-                                        </label>
-                                        <input
-                                            type="number"
-                                            id="quantityEnd"
-                                            className={clsx('text-input w-full py-[5px]', {
-                                                invalid:
-                                                    bacsicForm.touched.quantityEnd && bacsicForm.errors.quantityEnd,
-                                            })}
-                                            onChange={bacsicForm.handleChange}
-                                            onBlur={bacsicForm.handleBlur}
-                                            value={bacsicForm.values.quantityEnd}
-                                            name="quantityEnd"
-                                            placeholder="đến"
-                                        />
-                                        <span
-                                            className={clsx('text-sm text-red-500 opacity-0', {
-                                                'opacity-100':
-                                                    bacsicForm.touched.quantityEnd && bacsicForm.errors.quantityEnd,
-                                            })}
-                                        >
-                                            {bacsicForm.errors.quantityEnd || 'No message'}
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex">
-                                <Link to={'/product'} className="btn btn-red min-w-[100px]   basis-1/2 py-2">
-                                    <span className="pr-2">
-                                        <i className="fa-solid fa-circle-xmark"></i>
-                                    </span>
-                                    <span>Hủy </span>
-                                </Link>
-                                <button type="submit" className="btn btn-blue   min-w-[100px]  basis-1/2  py-2">
-                                    <span className="pr-2">
-                                        <i className="fa-solid fa-circle-plus"></i>
-                                    </span>
-                                    <span>Lọc</span>
-                                </button>
-                            </div>
-                        </Popover.Panel>
-                    </Popover>
                     <Link to="/product/views" className="btn btn-md btn-green">
                         <span className="pr-1">
                             <i className="fa-solid fa-circle-plus"></i>
@@ -385,7 +158,7 @@ function Products() {
                                 }
                             }
                         })
-                        ?.map((product, index) => (
+                        ?.map((product) => (
                             <tr
                                 key={product.id}
                                 className="flex cursor-pointer border-b border-slate-200 hover:bg-slate-100"
@@ -421,7 +194,7 @@ function Products() {
                                     className="flex w-28 items-center justify-end px-2 py-2"
                                     onClick={() => linkToDetail(product.id)}
                                 >
-                                    {product.price.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+                                    <PriceFormat>{product.price}</PriceFormat>
                                 </td>
                                 <td
                                     className="flex w-24 items-center justify-end px-2 py-2"
