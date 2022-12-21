6061cba397e13319c5b660932af35facff9bda7d
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-22 03:37:56 +0700

    update home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from 'react';
 
 function Home() {
-    return <h1>HOME</h1>;
+    return (
+        <div className="container flex h-full w-full items-center justify-center space-x-11">
+            <div className=" text-gray-600">
+                <p className="text-4xl font-extrabold">QUẢN LÝ</p>
+                <p className="text-3xl font-bold">CỬA HÀNG CÂY CẢNH</p>
+            </div>
+            <img className="w-[40vw] " src="/home-img.jpg" />
+        </div>
+    );
 }
 
 export default Home;
