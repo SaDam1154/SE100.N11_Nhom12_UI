5d0dd8844e4a1ee12db3f16031935da8ec710321
Đam Sa <20521154@ms.uit.edu.vn>
2022-12-22 04:10:55 +0700

    Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,8 +4,26 @@ function Home() {
     return (
         <div className="container flex h-full w-full items-center justify-center space-x-11">
             <div className=" text-gray-600">
-                <p className="text-4xl font-extrabold">QUẢN LÝ</p>
-                <p className="text-3xl font-bold">CỬA HÀNG CÂY CẢNH</p>
+                <p className="text-center text-4xl font-extrabold text-green-600">QUẢN LÝ</p>
+                <p className="text-center text-3xl font-bold text-green-600">CỬA HÀNG CÂY XANH</p>
+                <p className="text-xl font-bold">Nếu cần hỗ trợ kỹ thuật, vui lòng thực hiện một trong ba cách sau: </p>
+                <p className="text-lg ">
+                    1. Truy cập{' '}
+                    <a href="https://forum.uit.edu.vn/" className="underline hover:text-blue-600">
+                        https://forum.uit.edu.vn/
+                    </a>{' '}
+                    và gửi yêu cầu hỗ trợ.
+                </p>
+                <p className="text-lg">
+                    2. Gửi email cho phòng kỹ thuật: <span className="underline">20521154@gm.uit.edu.vn</span>
+                    {'.'}
+                </p>
+                <p className="text-lg ">
+                    3. Gọi HOTLINE hỗ trợ khách hàng: <span className="underline">0365011369</span>
+                    {', '}
+                    <span className="underline">0523011333</span>
+                    {'.'}
+                </p>
             </div>
             <img className="w-[40vw] " src="/home-img.jpg" />
         </div>
