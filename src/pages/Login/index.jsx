661f4889e5619ce4431f6ec5c24b32deeaa7595d
Đam Sa <20521154@ms.uit.edu.vn>
2022-12-27 00:20:28 +0700

    login nhưng chỉ console

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,25 @@
+import * as Yup from 'yup';
+import { Formik, useFormik } from 'formik';
+
+import clsx from 'clsx';
+
+const validationSchema = Yup.object({
+    account: Yup.string().required('Vui lòng nhập tên tài tài khoản!'),
+    password: Yup.string().required('Vui lòng nhập nhập mật khẩu!'),
+});
+
 function Login() {
+    const bacsicForm = useFormik({
+        initialValues: {
+            account: '',
+            password: '',
+        },
+        validationSchema,
+        onSubmit: handleFormsubmit,
+    });
+    function handleFormsubmit() {
+        console.log(bacsicForm.values);
+    }
     return (
         <div>
             <section className="bg-gray-200 ">
@@ -13,74 +34,66 @@ function Login() {
                     </a>
                     <div className="w-full rounded-lg bg-white shadow    sm:max-w-md md:mt-0 xl:p-0">
                         <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900  md:text-2xl">
-                                Sign in to your account
+                            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900  md:text-2xl">
+                                Đăng nhập
                             </h1>
-                            <form className="space-y-4 md:space-y-6" action="#">
+                            <form className="space-y-1 md:space-y-2" onSubmit={bacsicForm.handleSubmit}>
                                 <div>
-                                    <label for="email" className="mb-2 block text-sm font-medium text-gray-900 ">
-                                        Your email
+                                    <label htmlFor="account" className="mb-2 block text-sm font-medium text-gray-900 ">
+                                        Tài khoản
                                     </label>
                                     <input
-                                        type="email"
-                                        name="email"
-                                        id="email"
-                                        className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900    sm:text-sm"
-                                        placeholder="name@company.com"
-                                        required=""
+                                        type="text"
+                                        name="account"
+                                        id="account"
+                                        className={clsx(
+                                            'focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900    sm:text-sm',
+                                            {
+                                                invalid: bacsicForm.touched.account && bacsicForm.errors.account,
+                                            }
+                                        )}
+                                        onChange={bacsicForm.handleChange}
+                                        onBlur={bacsicForm.handleBlur}
+                                        value={bacsicForm.values.account}
+                                        placeholder="Tên tài khoản"
                                     />
+                                    <span
+                                        className={clsx('text-sm text-red-500 opacity-0', {
+                                            'opacity-100': bacsicForm.touched.account && bacsicForm.errors.account,
+                                        })}
+                                    >
+                                        {bacsicForm.errors.account || 'No message'}
+                                    </span>
                                 </div>
                                 <div>
-                                    <label for="password" className="mb-2 block text-sm font-medium text-gray-900 ">
-                                        Password
+                                    <label htmlFor="password" className="mb-2 block text-sm font-medium text-gray-900 ">
+                                        Mật khẩu
                                     </label>
                                     <input
                                         type="password"
                                         name="password"
                                         id="password"
-                                        placeholder="••••••••"
+                                        onChange={bacsicForm.handleChange}
+                                        onBlur={bacsicForm.handleBlur}
+                                        value={bacsicForm.values.password}
+                                        placeholder="Mật khẩu của bạn"
                                         className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900     sm:text-sm"
-                                        required=""
                                     />
+                                    <span
+                                        className={clsx('text-sm text-red-500 opacity-0', {
+                                            'opacity-100': bacsicForm.touched.password && bacsicForm.errors.password,
+                                        })}
+                                    >
+                                        {bacsicForm.errors.password || 'No message'}
+                                    </span>
                                 </div>
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-start">
-                                        <div className="flex h-5 items-center">
-                                            <input
-                                                id="remember"
-                                                aria-describedby="remember"
-                                                type="checkbox"
-                                                className="focus:ring-3 focus:ring-primary-300  h-4 w-4 rounded border border-gray-300 bg-gray-50  "
-                                                required=""
-                                            />
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label for="remember" className="text-gray-500 ">
-                                                Remember me
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <a href="#" className="text-primary-600  text-sm font-medium hover:underline">
-                                        Forgot password?
-                                    </a>
-                                </div>
+
                                 <button
                                     type="submit"
                                     className="btn btn-blue focus:ring-primary-300  w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
                                 >
-                                    Sign in
+                                    Đăng nhập
                                 </button>
-                                <div class="space-x-2 py-6">
-                                    <span class="inline-flex h-10 w-10 items-center justify-center rounded-full border-2 border-white text-lg font-bold">
-                                        f
-                                    </span>
-                                    <span class="inline-flex h-10 w-10 items-center justify-center rounded-full border-2 border-white text-lg font-bold">
-                                        G+
-                                    </span>
-                                    <span class="inline-flex h-10 w-10 items-center justify-center rounded-full border-2 border-white text-lg font-bold">
-                                        in
-                                    </span>
-                                </div>
                             </form>
                         </div>
                     </div>
