953c7bada29f602867d8191e7b678bbd8dbba9c7
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-30 11:50:34 +0700

    authorazation

--- src/layouts/components/Sidebar/GroupMenu.jsx
+++ src/layouts/components/Sidebar/GroupMenu.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link, NavLink, useLocation, useMatch } from 'react-router-dom';
 import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import { accountSelector } from '../../../redux/selectors';
 
 function GroupMenu({ groupMenu }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,8 +15,40 @@ function GroupMenu({ groupMenu }) {
     const pathFirst = pathname.split('/')[1];
     const mainPath = groupMenu.main.link?.split('/')[1];
 
+    const account = useSelector(accountSelector);
+
+    function isHiddenItem(functionName) {
+        if (!account) {
+            return true;
+        }
+        if (!functionName) {
+            return false;
+        }
+        const findResult = account?.functions?.find((_func) => _func?.name === functionName);
+        console.log(account?.functions, functionName, findResult);
+        if (findResult) {
+            return false;
+        }
+        return true;
+    }
+
+    function isHiddenParent() {
+        if (!groupMenu.children) {
+            return isHiddenItem(groupMenu.main?.functionName);
+        } else {
+            const filterResult = groupMenu.children?.filter((item) => !isHiddenItem(item.functionName));
+            if (filterResult.length === 0) {
+                return true;
+            }
+        }
+    }
+
     return (
-        <li>
+        <li
+            className={clsx({
+                '!hidden': isHiddenParent(),
+            })}
+        >
             <MainComp
                 className={clsx(
                     'flex cursor-pointer select-none  items-center justify-between rounded-md px-4 py-3 text-white hover:bg-blue-400',
@@ -45,6 +79,7 @@ function GroupMenu({ groupMenu }) {
                             className={({ isActive }) =>
                                 clsx('flex cursor-pointer items-center pl-10 pr-3 text-white hover:underline', {
                                     'font-semibold underline': isActive,
+                                    '!hidden': isHiddenItem(item?.functionName),
                                 })
                             }
                             to={groupMenu.main.link + item.link}
