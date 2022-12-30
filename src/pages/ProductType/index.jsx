6657bbcb061119963459cf0b5e155c76a34e0d59
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-30 15:08:15 +0700

    delete product type dialog

--- src/pages/ProductType/index.jsx
+++ src/pages/ProductType/index.jsx
@@ -40,31 +40,17 @@ function ProductType() {
     const [search, setSearch] = useState('');
     const [productTypes, setProductTypes] = useState([]);
     const navigate = useNavigate();
-    // const showDeleteNoti = () => toast.info('Xóa sản phẩm thành công!');
-    // const showErorrNoti = () => toast.error('Có lỗi xảy ra!');
-    // function deleteProduct(id) {
-    //     fetch('http://localhost:5000/api/product/' + id, {
-    //         method: 'DELETE',
-    //     })
-    //         .then((res) => res.json())
-    //         .then((resJson) => {
-    //             if (resJson) {
-    //                 showDeleteNoti();
-    //                 console.log('xóa');
-    //                 callApi();
-    //             } else {
-    //                 showErorrNoti();
-    //             }
-    //         })
-    //         .catch(() => {
-    //             showErorrNoti();
-    //         });
-    // }
+
+    const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+    const [deletingProductTypeId, setDeletingProductTypeId] = useState(null);
+    const showDeleteNoti = () => toast.info('Xóa loại sản phẩm thành công!');
+    const showErorrNoti = () => toast.error('Có lỗi xảy ra!');
+
     useEffect(() => {
-        callApi();
+        getProductTypes();
     }, []);
 
-    function callApi() {
+    function getProductTypes() {
         fetch('http://localhost:5000/api/product-type')
             .then((res) => res.json())
             .then((resJson) => {
@@ -80,121 +66,188 @@ function ProductType() {
         navigate('/product-type/detail/' + id);
     }
 
-    return (
-        <div className="container">
-            <div className="flex space-x-4">
-                {/* tite + reload btn */}
-                <div className="flex">
-                    <label className="text-2xl font-bold text-slate-800">Danh sách loại cây</label>
-                    <button type="button" className="ml-3 text-gray-800 hover:underline" onClick={() => callApi()}>
-                        <span className="font-sm pr-1">
-                            <i className="fa fa-refresh" aria-hidden="true"></i>
-                        </span>
-                        <span className="">Tải lại</span>
-                    </button>
-                </div>
+    function deleteProductType(id) {
+        fetch('http://localhost:5000/api/product-type/' + id, {
+            method: 'DELETE',
+        })
+            .then((res) => res.json())
+            .then((resJson) => {
+                setShowDeleteDialog(false);
+                if (resJson) {
+                    showDeleteNoti();
+                    console.log('xóa');
+                    getProductTypes();
+                } else {
+                    showErorrNoti();
+                }
+            })
+            .catch(() => {
+                showErorrNoti();
+                setShowDeleteDialog(false);
+            });
+    }
 
-                {/* Action group */}
-                <div className="flex grow">
-                    {/* Search */}
-                    <div className="mr-2 flex grow">
-                        <input
-                            type="text"
-                            className="text-input grow"
-                            onChange={(e) => {
-                                setSearch(e.target.value);
-                            }}
-                            placeholder="Tìm kiếm loại sản phẩm"
-                        />
+    return (
+        <>
+            <div className="container">
+                <div className="flex space-x-4">
+                    {/* tite + reload btn */}
+                    <div className="flex">
+                        <label className="text-2xl font-bold text-slate-800">Danh sách loại cây</label>
+                        <button
+                            type="button"
+                            className="ml-3 text-gray-800 hover:underline"
+                            onClick={() => getProductTypes()}
+                        >
+                            <span className="font-sm pr-1">
+                                <i className="fa fa-refresh" aria-hidden="true"></i>
+                            </span>
+                            <span className="">Tải lại</span>
+                        </button>
                     </div>
 
-                    <Link to="/product-type/add" className="btn btn-md btn-green">
-                        <span className="pr-1">
-                            <i className="fa fa-share"></i>
-                        </span>
-                        <span>Thêm loại cây mới</span>
-                    </Link>
+                    {/* Action group */}
+                    <div className="flex grow">
+                        {/* Search */}
+                        <div className="mr-2 flex grow">
+                            <input
+                                type="text"
+                                className="text-input grow"
+                                onChange={(e) => {
+                                    setSearch(e.target.value);
+                                }}
+                                placeholder="Tìm kiếm loại sản phẩm"
+                            />
+                        </div>
+
+                        <Link to="/product-type/add" className="btn btn-md btn-green">
+                            <span className="pr-1">
+                                <i className="fa fa-share"></i>
+                            </span>
+                            <span>Thêm loại cây mới</span>
+                        </Link>
+                    </div>
                 </div>
+
+                {/* LIST */}
+                <table className="mt-8 w-full">
+                    <thead className="w-full rounded bg-blue-500 text-white">
+                        <tr className="flex h-11 w-full">
+                            <th className="flex w-20 items-center justify-end px-2">Mã số</th>
+                            <th className="flex flex-[1] items-center justify-start pl-28">Tên loại sản phẩm</th>
+                            <th className="flex flex-[1] items-center justify-start px-2">Ngày thêm</th>
+                            <th className="flex w-[200px] items-center justify-center px-2"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody className="flex h-[75vh] w-full flex-col" style={{ overflowY: 'overlay' }}>
+                        {productTypes
+                            ?.filter((productType) => {
+                                if (search === '') {
+                                    return productType;
+                                } else {
+                                    if (
+                                        removeVietnameseTones(productType.name.toLowerCase()).includes(
+                                            removeVietnameseTones(search.toLowerCase())
+                                        )
+                                    ) {
+                                        var id = productType.id.toString();
+                                        return productType.id.toString().includes(id);
+                                    }
+                                }
+                            })
+                            ?.map((productType) => (
+                                <tr
+                                    key={productType.id}
+                                    className="flex cursor-pointer border-b border-slate-200 hover:bg-slate-100"
+                                >
+                                    <td
+                                        className="flex w-20 items-center justify-center px-2"
+                                        onClick={() => linkToDetail(productType.id)}
+                                    >
+                                        {productType.id}
+                                    </td>
+
+                                    <td
+                                        className="flex flex-[1] items-center justify-start pl-28"
+                                        onClick={() => linkToDetail(productType.id)}
+                                    >
+                                        {productType.name}
+                                    </td>
+
+                                    <td
+                                        className="flex flex-[1] items-center justify-start px-2"
+                                        onClick={() => linkToDetail(productType.id)}
+                                    >
+                                        {moment(productType.createdAt).format('HH:mm:ss DD/MM/YYYY ')}
+                                    </td>
+                                    <td className="flex w-[200px] items-center justify-center px-2 py-2">
+                                        <div className="flex justify-end">
+                                            <Link
+                                                to={'/product-type/update/' + productType.id}
+                                                className="btn btn-sm btn-blue"
+                                            >
+                                                <span className="pr-1">
+                                                    <i className="fa-solid fa-pen-to-square"></i>
+                                                </span>
+                                                <span>Sửa</span>
+                                            </Link>
+                                            <button
+                                                className="btn btn-sm btn-red"
+                                                onClick={() => {
+                                                    {
+                                                        setShowDeleteDialog(true);
+                                                        setDeletingProductTypeId(productType.id);
+                                                    }
+                                                }}
+                                            >
+                                                <span className="pr-1">
+                                                    <i className="fa-solid fa-circle-xmark"></i>
+                                                </span>
+                                                <span>Xoá</span>
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
             </div>
 
-            {/* LIST */}
-            <table className="mt-8 w-full">
-                <thead className="w-full rounded bg-blue-500 text-white">
-                    <tr className="flex h-11 w-full">
-                        <th className="flex w-20 items-center justify-end px-2">Mã số</th>
-                        <th className="flex flex-[1] items-center justify-start pl-28">Tên loại sản phẩm</th>
-                        <th className="flex flex-[1] items-center justify-start px-2">Ngày thêm</th>
-                        <th className="flex w-[200px] items-center justify-center px-2"></th>
-                    </tr>
-                </thead>
-
-                <tbody className="flex h-[75vh] w-full flex-col" style={{ overflowY: 'overlay' }}>
-                    {productTypes
-                        ?.filter((product) => {
-                            if (search === '') {
-                                return product;
-                            } else {
-                                if (
-                                    removeVietnameseTones(product.name.toLowerCase()).includes(
-                                        removeVietnameseTones(search.toLowerCase())
-                                    )
-                                ) {
-                                    var id = product.id.toString();
-                                    return product.id.toString().includes(id);
-                                }
-                            }
-                        })
-                        ?.map((product) => (
-                            <tr
-                                key={product.id}
-                                className="flex cursor-pointer border-b border-slate-200 hover:bg-slate-100"
+            {/* DELETE DIALOG */}
+            <div
+                className={clsx(
+                    'fixed inset-0 z-[99999] hidden items-center justify-center bg-black/20 opacity-0 transition-opacity',
+                    {
+                        '!flex !opacity-100': showDeleteDialog,
+                    }
+                )}
+            >
+                <div className="">
+                    <div className="min-w-[160px] max-w-[400px] rounded-lg bg-white p-6">
+                        <div className="text-clr-text-dark font-bold">Bạn có chắc chắn muốn xoá không?</div>
+                        <p className="mt-4">Lưu ý: Bạn không thể không phục lại sau khi xoá!</p>
+                        <div className="mt-4 flex">
+                            <button
+                                className="btn btn-blue btn-md"
+                                onClick={() => {
+                                    setDeletingProductTypeId(null);
+                                    setShowDeleteDialog(false);
+                                }}
                             >
-                                <td
-                                    className="flex w-20 items-center justify-center px-2"
-                                    onClick={() => linkToDetail(product.id)}
-                                >
-                                    {product.id}
-                                </td>
-
-                                <td
-                                    className="flex flex-[1] items-center justify-start pl-28"
-                                    onClick={() => linkToDetail(product.id)}
-                                >
-                                    {product.name}
-                                </td>
-
-                                <td
-                                    className="flex flex-[1] items-center justify-start px-2"
-                                    onClick={() => linkToDetail(product.id)}
-                                >
-                                    {moment(product.createdAt).format('HH:mm:ss DD/MM/YYYY ')}
-                                </td>
-                                <td className="flex w-[200px] items-center justify-center px-2 py-2">
-                                    <div className="flex justify-end">
-                                        <Link to={'/product-type/update/' + product.id} className="btn btn-sm btn-blue">
-                                            <span className="pr-1">
-                                                <i className="fa-solid fa-pen-to-square"></i>
-                                            </span>
-                                            <span>Sửa</span>
-                                        </Link>
-                                        <button
-                                            className="btn btn-sm btn-red"
-                                            // onClick={() =>
-                                            //     deleteProduct(product.id)
-                                            // }
-                                        >
-                                            <span className="pr-1">
-                                                <i className="fa-solid fa-circle-xmark"></i>
-                                            </span>
-                                            <span>Xoá</span>
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                </tbody>
-            </table>
-        </div>
+                                Quay lại
+                            </button>
+                            <button
+                                className="btn btn-md btn-red"
+                                onClick={() => deleteProductType(deletingProductTypeId)}
+                            >
+                                Xoá
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
